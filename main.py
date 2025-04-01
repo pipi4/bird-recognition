249e7d64c64c2d5bedec2496a7437df16d271011
Haozilla <haozilla@icloud.com>
2025-04-01 11:04:45 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from uvicorn import Server, Config
+from routers import router
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def get_index():
+    return {"message": "Welcome to the FastAPI application!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    server = Server(Config(app, host="0.0.0.0", port=port))
+    server.run()
